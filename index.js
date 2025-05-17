bf30148b6e6d1ffcd6378ae3606cde4f9fe9b804
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-05-18 01:40:04 +0600

    Fifth section complete

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const scroll = new LocomotiveScroll({
 });
 
 
+// Hover image show section 
 let elemContainer = document.querySelector("#elem-container");
 let fixedImage = document.querySelector("#fixed-image");
 
 elemContainer.addEventListener( "mouseenter", () => {
     fixedImage.style.display = "block";
 });
-elemContainer.addEventListener( "mousemove", () => {
+
+elemContainer.addEventListener( "mouseleave", () => {
     fixedImage.style.display = "none";
 });
+
+
+// Add image inside fixed image card
+let allElem = document.querySelectorAll(".elem");
+ 
+allElem.forEach((item, i) => {
+    item.addEventListener("mouseenter", () => {
+        let image = item.getAttribute("data-image");
+         fixedImage.style.backgroundImage = `url(${image})`
+    })
+})

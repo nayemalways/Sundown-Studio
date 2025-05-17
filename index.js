d906216bff2ce7403b120aed3842716204df1a52
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-05-17 21:04:16 +0600

    fourth section js logic added

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#root'),
     smooth: true
 });
+
+
+let elemContainer = document.querySelector("#elem-container");
+let fixedImage = document.querySelector("#fixed-image");
+
+elemContainer.addEventListener( "mouseenter", () => {
+    fixedImage.style.display = "block";
+});
+elemContainer.addEventListener( "mousemove", () => {
+    fixedImage.style.display = "none";
+});
